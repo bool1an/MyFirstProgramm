fdc5d1255df91313ce1e842c0f766664d93c90f8
denn02 <denis.knyazev04@mail.ru>
2020-02-06 18:37:46 +0300

    first commit

--- HomeWork.2.4.1/app/src/main/java/ru/samsung/itschool/book/MainActivity.java
+++ HomeWork.2.4.1/app/src/main/java/ru/samsung/itschool/book/MainActivity.java
@@ -2,6 +2,7 @@ package ru.samsung.itschool.book;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -13,6 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void run(View v) {
+        EditText a = findViewById(R.id.a);
+        String strA = a.getText().toString();
+        int numA = Integer.parseInt(strA);
+
+        EditText b = findViewById(R.id.b);
+        String strB = b.getText().toString();
+        int numB = Integer.parseInt(strB);
+
+        EditText c = findViewById(R.id.c);
+        String strC = c.getText().toString();
+        int numC = Integer.parseInt(strC);
     }
 
 
