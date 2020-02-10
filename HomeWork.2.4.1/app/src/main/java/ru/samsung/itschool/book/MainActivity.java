6bf7f4fc611002b92b1ae9fbc9def9de0590944b
denn02 <denis.knyazev04@mail.ru>
2020-02-10 20:31:44 +0500

    Debugged

--- HomeWork.2.4.1/app/src/main/java/ru/samsung/itschool/book/MainActivity.java
+++ HomeWork.2.4.1/app/src/main/java/ru/samsung/itschool/book/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,17 +19,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void run(View v) {
-        EditText a = findViewById(R.id.a);
-        String strA = a.getText().toString();
-        int numA = Integer.parseInt(strA);
+        EditText A = findViewById(R.id.a);
+        String strA = A.getText().toString();
+        double a = Double.parseDouble(strA);
 
-        EditText b = findViewById(R.id.b);
-        String strB = b.getText().toString();
-        int numB = Integer.parseInt(strB);
+        EditText B = findViewById(R.id.b);
+        String strB = B.getText().toString();
+        double b = Double.parseDouble(strB);
 
-        EditText c = findViewById(R.id.c);
-        String strC = c.getText().toString();
-        int numC = Integer.parseInt(strC);
+        EditText C = findViewById(R.id.c);
+        String strC = C.getText().toString();
+        double c = Double.parseDouble(strC);
+
+        String value;
+        String StrX1 = "none";
+        String StrX2 = "none";
+        double D = b * b - 4 * a * c;
+
+        if (D > 0) {
+           double x1 = (-b + Math.sqrt(D)) / (2 * a);
+           StrX1 = Double.toString(x1);
+           value = StrX1;
+        }
+        if (D >= 0) {
+            double x2 = (-b - Math.sqrt(D)) / (2 * a);
+            StrX2 = Double.toString(x2);
+            value = StrX1 + " " + StrX2;
+        } else {
+            value = "none";
+        }
+        if (a == 0 && b == 0 && c == 0) {
+            value = "any";
+        }
+        TextView textViewRes = findViewById(R.id.res);
+        textViewRes.setText(value);
     }
 
 
